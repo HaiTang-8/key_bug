c9fb2769bff89bd52db70c26ecf99121994f0d13
HaiTang-8 <3166503950@qq.com>
2023-01-20 21:29:05 +0800

    init

---
+++ src/components/Index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <el-button @click="previewIsShow = true;">预览</el-button>
+
+    <el-dialog title="图片预览" width="95%" top="1%" :visible.sync="previewIsShow">
+      <el-button @click="flushImg">刷新</el-button>
+      <div :key="key" style="margin-top: 10px">
+        <img src="https://picsum.photos/2500/1667" class="previewImg" alt=""/>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  data() {
+    return {
+      key: 0,
+      previewIsShow: false,
+    }
+  },
+  methods: {
+    // 刷新图片
+    flushImg() {
+      this.key += 1;
+    },
+  }
+}
+</script>
+
+<style scoped>
+.previewImg {
+  display: block;
+  -webkit-user-select: none;
+  margin: auto;
+  cursor: zoom-in;
+  background-color: hsl(0, 0%, 90%);
+  transition: background-color 300ms;
+  height: 100%;
+  width: 100%;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import 'element-ui/lib/theme-chalk/index.css'
+
+import {Button, Dialog} from 'element-ui';
+
+Vue.component(Button.name, Button);
+Vue.component(Dialog.name, Dialog);
+
 Vue.config.productionTip = false
 
 new Vue({
